47e87fc1dbeed14921bc32779262fcb725fe16f4
kuhakuni <afif.haidar@gmail.com>
2021-11-26 08:39:46 +0700

    Fix scroll to element

--- assets/js/script.js
+++ assets/js/script.js
@@ -163,7 +163,19 @@
 				50
 			);
 		});
-
+		/**
+		 * Scroll to element
+		 */
+		$('a[href^="#"]').on("click", function (e) {
+			e.preventDefault();
+			$("html, body").animate(
+				{
+					scrollTop: $($(this).attr("href")).offset().top - 20,
+				},
+				50,
+				"linear"
+			);
+		});
 		/**
 		 * Testimonials slider
 		 */
